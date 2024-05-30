63c780c8aa7aa88ef07dce1630e776b9794bc1ba
Virginia Dooley <vcdooley@gmail.com>
2024-05-30 21:41:47 +0100

    Create NationalPartyImporter

--- wcivf/apps/parties/importers.py
+++ wcivf/apps/parties/importers.py
@@ -4,9 +4,10 @@ from collections import namedtuple
 from core.helpers import twitter_username
 from core.mixins import ReadFromFileMixin, ReadFromUrlMixin
 from elections.models import Election, PostElection
-from parties.models import LocalParty, Manifesto, Party
+from parties.models import LocalParty, Manifesto, NationalParty, Party
 
 LocalElection = namedtuple("LocalElection", ["date", "csv_files"])
+NationalElection = namedtuple("NationalElection", ["date", "csv_files"])
 
 
 class LocalPartyImporter(ReadFromUrlMixin, ReadFromFileMixin):
@@ -150,11 +151,181 @@ class LocalPartyImporter(ReadFromUrlMixin, ReadFromFileMixin):
         """
         return row.get("Local party name", row.get("Party name"))
 
+    def get_country(self, election_type):
+        country_mapping = {
+            "local": "Local",
+            "senedd": "Wales",
+            "sp": "Scotland",
+        }
+        return country_mapping.get(election_type, "UK")
+
     def import_parties(self):
         """
         This is the main action that is run by the class. First existing
         LocalParty objects for an election are deleted. Then it validates the
         row contains data we can use. Then adds a LocalParty object for each
+        parent party.
+        """
+        self.delete_parties()
+        self.delete_manifestos()
+
+
+class NationalPartyImporter(ReadFromUrlMixin, ReadFromFileMixin):
+    JOINT_PARTIES = [["party:53", "joint-party:53-119"]]
+
+    def __init__(
+        self,
+        election,
+        force_update=False,
+        from_file=False,
+    ):
+        self.election = election
+        self.force_update = force_update
+        self.read_from = getattr(self, "read_from_url")
+        if from_file:
+            self.read_from = getattr(self, "read_from_file")
+
+    def write(self, msg):
+        """
+        Ensures all outputs include a new line
+        """
+        sys.stdout.write(f"{msg}\n")
+
+    def delete_parties(self):
+        """
+        Deletes NationalParty objects associated with elections for the given
+        election date
+        """
+        count, _ = NationalParty.objects.filter(
+            file_url__in=self.election.csv_files,
+        ).delete()
+
+        self.write(f"Deleted {count} local parties")
+
+    def delete_manifestos(self):
+        count, _ = Manifesto.objects.filter(
+            file_url__in=self.election.csv_files,
+        ).delete()
+
+        self.write(f"Deleted {count} manifestos")
+
+    def current_elections(self):
+        """
+        Checks for current Election objects with the given date
+        """
+        if self.force_update:
+            return True
+
+        return Election.objects.filter(
+            current=True, election_date=self.election.date
+        ).exists()
+
+    def get_party_list_from_party_id(self, party_id):
+        """
+        Checks for any joint parties for the given party id
+        """
+        party_id = f"party:{party_id}"
+
+        for party_list in self.JOINT_PARTIES:
+            if party_id in party_list:
+                return party_list
+        return [party_id]
+
+    def get_parties(self, party_id):
+        """
+        Return a QuerySet of Party objects for the party_id
+        """
+        party_list = self.get_party_list_from_party_id(party_id)
+        return Party.objects.filter(party_id__in=party_list)
+
+    def get_ballots(self, election_id, parties):
+        """
+        First checks if the election_id is a special case or is not a full
+        election ID e.g. it is made of only two parts such as local.2022-05-05.
+        In this case we return all ballots for the type of election and the
+        date. Otherwise attempts to find a single PostElection object for the
+        election_id. If this does not exist it may be that the ID is the slug
+        of an Election object so we return all ballots related to the Election.
+        """
+
+        special_cases = ["senedd", "sp", "gla"]
+        election_id_list = election_id.split(".")
+        election_type = election_id_list[0]
+        if len(election_id_list) == 2 or election_type in special_cases:
+            ballots = PostElection.objects.filter(
+                ballot_paper_id__startswith=f"{election_type}.",
+                election__election_date=self.election.date,
+            ).exclude(localparty__parent__in=parties)
+        else:
+            ballots = PostElection.objects.filter(ballot_paper_id=election_id)
+
+        if not ballots:
+            # This might be an election ID, in that case,
+            # apply the row to all post elections without
+            # info already
+            ballots = PostElection.objects.filter(
+                election__slug=election_id
+            ).exclude(localparty__parent__in=parties)
+
+        return ballots.filter(personpost__party__in=parties)
+
+    def add_national_party(self, row, party, ballots, file_url):
+        """
+        Takes a row of data, a Party, and a QuerySet of at least one
+        PostElection objects, and creates a NationalParty for each of the ballots.
+        """
+
+        name = self.get_name(row=row)
+        # only create local parties for ballots where a candidate is standing
+        # for the parent party
+        ballots = ballots.filter(personpost__party=party)
+
+        for post_election in ballots.distinct():
+            defaults = {
+                "name": name,
+                "facebook_page": row["Facebook"],
+                "instagram": row["Instagram"],
+                "homepage": row["Website"],
+                "email": row["Email"],
+                "youtube_profile_url": row.get("Youtube profile", "").strip(),
+                "contact_page_url": row.get("Contact page", "").strip(),
+                "party_political_broadcast": row.get(
+                    "Party political broadcast", ""
+                ).strip(),
+                "file_url": file_url,
+                "is_national": True,
+            }
+
+            _, created = NationalParty.objects.update_or_create(
+                parent=party, post_election=post_election, defaults=defaults
+            )
+            msg = "Created" if created else "Updated"
+            self.write(
+                f"{msg} Local Party object for {party.party_name} in {post_election.ballot_paper_id}"
+            )
+
+        self.write(f"Imported National Party objects for {name}")
+
+    def get_name(self, row):
+        """
+        The sheet for Scottish/Welsh/GLA uses "Party Name" header so check for
+        both
+        """
+        return row.get("National party name", row.get("Party Name"))
+
+    def get_country(self, election_type):
+        country_mapping = {
+            "local": "Local",
+            "senedd": "Wales",
+            "sp": "Scotland",
+        }
+        return country_mapping.get(election_type, "UK")
+
+    def import_parties(self):
+        """
+        This is the main action that is run by the class. First existing
+        NationalParty objects for an election are deleted. Then it validates the
+        row contains data we can use. Then adds a NationalParty object for each
         parent party.
         """
         self.delete_parties()
@@ -190,7 +361,7 @@ class LocalPartyImporter(ReadFromUrlMixin, ReadFromFileMixin):
                 # store the PK's in a list as something strange is
                 # happening later on - when trying to access
                 # objects from the 'ballots' queryset after the
-                # add_local_party method has been called, None is
+                # add_general_election_party method has been called, None is
                 # returned instead of the object.
                 # This resulted in the 'elections' queryset also
                 # being empty. Storing the PK's in a list gets round
@@ -200,7 +371,7 @@ class LocalPartyImporter(ReadFromUrlMixin, ReadFromFileMixin):
                     postelection__in=ballot_pks
                 ).distinct()
                 for party in parties:
-                    self.add_local_party(row, party, ballots, file_url)
+                    self.add_national_party(row, party, ballots, file_url)
                     manifesto_web = row.get("Manifesto Website URL", "").strip()
                     manifesto_pdf = row.get("Manifesto PDF URL", "").strip()
                     if not any([manifesto_web, manifesto_pdf]):
@@ -210,18 +381,10 @@ class LocalPartyImporter(ReadFromUrlMixin, ReadFromFileMixin):
                     for election in elections:
                         self.add_manifesto(row, party, election, file_url)
 
-    def get_country(self, election_type):
-        country_mapping = {
-            "local": "Local",
-            "senedd": "Wales",
-            "sp": "Scotland",
-        }
-        return country_mapping.get(election_type, "UK")
-
     def add_manifesto(self, row, party, election, file_url):
         manifesto_web = row.get("Manifesto Website URL", "").strip()
         manifesto_pdf = row.get("Manifesto PDF URL", "").strip()
-        country = self.get_country(election_type=election.election_type)
+
         language = row.get("Manifesto Language", "English").strip()
         easy_read_url = row.get("Manifesto Easy Read PDF", "").strip()
         if any([manifesto_web, manifesto_pdf]):
@@ -231,6 +394,7 @@ class LocalPartyImporter(ReadFromUrlMixin, ReadFromFileMixin):
                 "easy_read_url": easy_read_url,
                 "file_url": file_url,
             }
+            country = self.get_country(election_type=election.election_type)
             manifesto_obj, created = Manifesto.objects.update_or_create(
                 election=election,
                 party=party,
--- wcivf/apps/parties/tests/test_importers.py
+++ wcivf/apps/parties/tests/test_importers.py
@@ -2,8 +2,13 @@ import sys
 
 import pytest
 from elections.models import Election, PostElection, PostElectionQuerySet
-from parties.importers import LocalElection, LocalPartyImporter
-from parties.models import LocalParty
+from parties.importers import (
+    LocalElection,
+    LocalPartyImporter,
+    NationalElection,
+    NationalPartyImporter,
+)
+from parties.models import LocalParty, NationalParty
 
 
 class TestLocalPartyImporter:
@@ -215,3 +220,200 @@ class TestLocalPartyImporter:
             expected = case[1]
             with subtests.test(msg=case[0]):
                 assert importer.get_country(election_slug) == expected
+
+
+class TestNationalPartyImporter:
+    @pytest.fixture
+    def national_election(self):
+        return NationalElection(
+            date="2024-07-04",
+            csv_files=[
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vS0GJ6wED4d87K1AMXTxxfYl1L-RwjuH-DY1lcwAH9Wj8MChrxPRVDXqc1dzMW8sVdGDLXcBi0usnDl/pub?gid=0&single=true&output=csv"
+            ],
+        )
+
+    @pytest.fixture
+    def importer(self, national_election):
+        return NationalPartyImporter(election=national_election)
+
+    @pytest.fixture
+    def row(self):
+        return {
+            "Party Name": "Labour Ecclesall",
+            "party_id": "53",
+            "election_id": "local.sheffield.2021-05-06",
+            "Instagram": "https://instagram.com/example",
+            "Facebook": "https://facebook.com/example",
+            "Website": "https://example.com",
+            "Email": "email@example.com",
+            "Party Political Broadcast URL": "http://example.com/ppb",
+            "Manifesto Website URL": "http://example.com/manifesto",
+            "Manifesto PDF URL": "http://example.com/manifesto.pdf",
+        }
+
+    def test_init(self, national_election):
+        importer = NationalPartyImporter(election=national_election)
+        assert importer.election == national_election
+        assert importer.force_update is False
+        assert importer.read_from == importer.read_from_url
+
+    def test_init_from_file(self, mocker):
+        election = mocker.Mock()
+        importer = NationalPartyImporter(election=election, from_file=True)
+        assert importer.election == election
+        assert importer.force_update is False
+        assert importer.read_from == importer.read_from_file
+
+    def test_write(self, importer, mocker):
+        mocker.patch.object(sys.stdout, "write")
+        importer.write("Foo")
+        sys.stdout.write.assert_called_once_with("Foo\n")
+
+    def test_delete_parties(self, importer, mocker):
+        filter = mocker.MagicMock()
+        filter.return_value.delete.return_value = (0, {})
+        mocker.patch.object(NationalParty.objects, "filter", new=filter)
+        importer.delete_parties()
+        filter.assert_called_once_with(file_url__in=importer.election.csv_files)
+        filter.return_value.delete.assert_called_once()
+
+    def test_current_elections(self, importer):
+        importer.force_update = True
+        assert importer.current_elections() is True
+
+    @pytest.mark.parametrize("exists", [True, False])
+    def test_current_elections_dont_force(self, importer, mocker, exists):
+        filter = mocker.MagicMock()
+        filter.return_value.exists.return_value = exists
+        mocker.patch.object(Election.objects, "filter", new=filter)
+        assert importer.current_elections() is exists
+        filter.assert_called_once_with(
+            current=True, election_date=importer.election.date
+        )
+        filter.return_value.exists.assert_called_once()
+
+    def test_get_ballots_filters_by_ballot_paper_id(self, importer, mocker):
+        mock_qs = mocker.MagicMock(spec=PostElectionQuerySet)
+        mocker.patch.object(
+            PostElection.objects, "filter", return_value=mock_qs
+        )
+        importer.get_ballots(election_id="Foo", parties=["parties"])
+        PostElection.objects.filter.assert_called_once_with(
+            ballot_paper_id="Foo",
+        )
+        mock_qs.filter.assert_called_once_with(
+            personpost__party__in=["parties"]
+        )
+
+    def test_get_ballots_filter_by_election_slug(self, importer, mocker):
+        mock = mocker.MagicMock()
+        mock.__bool__.return_value = False
+        mocker.patch.object(PostElection.objects, "filter", return_value=mock)
+        importer.get_ballots(election_id="Foo", parties=["parties"])
+        PostElection.objects.filter.assert_called_with(
+            election__slug="Foo",
+        )
+        mock.exclude.return_value.filter.assert_called_once_with(
+            personpost__party__in=["parties"],
+        )
+
+    def test_import_parties_no_current_elections(self, importer, mocker):
+        mocker.patch.object(importer, "delete_parties")
+        mocker.patch.object(importer, "delete_manifestos")
+        mocker.patch.object(importer, "current_elections", return_value=False)
+        mocker.patch.object(importer, "read_from_url")
+
+        assert importer.import_parties() is None
+        importer.delete_parties.assert_called_once()
+        importer.delete_manifestos.assert_called_once()
+        importer.current_elections.assert_called_once()
+        importer.read_from_url.assert_not_called()
+
+    def test_import_parties_runs(self, importer, row, mocker):
+        mocker.patch.object(importer, "delete_parties")
+        mocker.patch.object(importer, "delete_manifestos")
+        mocker.patch.object(importer, "current_elections")
+
+        party = mocker.MagicMock()
+        mocker.patch.object(importer, "get_parties", return_value=[party])
+
+        ballots = mocker.MagicMock()
+        ballots.values_list.return_value = [1, 2, 3, 4]
+        mocker.patch.object(importer, "get_ballots", return_value=ballots)
+
+        mocker.patch.object(importer, "read_from", return_value=[row])
+        mock_elections = mocker.MagicMock()
+        mock_elections.distinct.return_value = ["election_obj"]
+        mocker.patch.object(importer, "add_national_party")
+        mocker.patch.object(importer, "add_manifesto")
+
+        mocker.patch.object(
+            Election.objects, "filter", return_value=mock_elections
+        )
+
+        # actual call to do the import
+        importer.import_parties()
+
+        # assert what we expect to have been called was called
+        importer.delete_parties.assert_called_once()
+        importer.delete_manifestos.assert_called_once()
+        importer.current_elections.assert_called_once()
+        file_url = importer.election.csv_files[0]
+        importer.add_national_party.assert_called_once_with(
+            row, party, ballots, file_url
+        )
+
+        importer.add_manifesto.assert_called_once_with(
+            row, party, "election_obj", file_url
+        )
+        Election.objects.filter.assert_called_once_with(
+            postelection__in=[1, 2, 3, 4]
+        )
+
+    def test_add_national_party(self, importer, row, mocker):
+        party = mocker.MagicMock(name="Example National Party")
+        mocker.patch.object(
+            NationalParty.objects,
+            "update_or_create",
+            return_value=(party, True),
+        )
+        party = mocker.MagicMock()
+        post_election = mocker.MagicMock()
+        ballots = mocker.MagicMock()
+        ballots.filter.return_value.distinct.return_value = [post_election]
+
+        file_url = importer.election.csv_files[0]
+        importer.add_national_party(
+            row=row, party=party, ballots=ballots, file_url=file_url
+        )
+
+        ballots.filter.assert_called_once_with(personpost__party=party)
+        NationalParty.objects.update_or_create.assert_called_once_with(
+            parent=party,
+            post_election=post_election,
+            defaults={
+                "name": row["Party Name"],
+                "instagram": row["Instagram"],
+                "facebook_page": row["Facebook"],
+                "homepage": row["Website"],
+                "email": row["Email"],
+                "youtube_profile_url": "",
+                "contact_page_url": "",
+                "party_political_broadcast": "",
+                "file_url": file_url,
+                "is_national": True,
+            },
+        )
+
+    def test_get_name(self, importer, subtests):
+        cases = [
+            ({"National party name": "Welsh Labour"}, "Welsh Labour"),
+            ({"Party Name": "Welsh Labour"}, "Welsh Labour"),
+            ({}, None),
+        ]
+        for case in cases:
+            row = case[0]
+            expected = case[1]
+            with subtests.test(msg=row):
+                result = importer.get_name(row)
+                assert result == expected
