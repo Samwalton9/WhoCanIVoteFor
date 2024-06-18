df4089c6df60ff819b57533bd349a172559fea4d
Sym Roe <sym.roe@talusdesign.co.uk>
2024-06-18 21:39:04 +0100

    Only use for hustings

--- wcivf/apps/core/db_routers.py
+++ wcivf/apps/core/db_routers.py
@@ -16,11 +16,15 @@ class FeedbackRouter(object):
 
 
 class PrincipleRDSRouter:
+    apps_that_use_principle_router = ["hustings"]
+
     def db_for_read(self, model, **hints):
         return "default"
 
     def db_for_write(self, model, **hints):
-        return "principle"
+        if model._meta.app_label in self.apps_that_use_principle_router:
+            return "principle"
+        return "default"
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         # Managed by CI / Lambda
